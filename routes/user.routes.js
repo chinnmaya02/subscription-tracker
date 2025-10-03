18f11d2de5e48adeb74e16a26e9e80fcde8a945e
Chinnmaya Karwal <omkarwal200@gmail.com>
2025-10-03 22:53:10 +0530

    Added authentication

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import { getUsers, getUser } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 //Get/users -> get all users
 //Get/users/id -> get user by id //123 412
 
-userRouter.get('/', (req, res) => res.send( {title: 'Get all users'}));
+userRouter.get('/', getUsers);
 
-userRouter.get('/:id', (req, res) => res.send( {title: 'Get user details'}));
+userRouter.get('/:id',authorize, getUser);
 
 userRouter.post('/', (req, res) => res.send( {title: 'Create new users'}));
 
